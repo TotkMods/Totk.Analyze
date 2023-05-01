140d4ae1ae38c71766969d0f6fe4360afb2c7e12
ArchLeaders <archparliament@gmail.com>
2023-05-01 15:40:24 -0700

    Create simple RESTBL parser

---
+++ Totk.Analyze/Formats/RESTBL.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace Totk.Analyze.Formats;
+
+public class RESTBL
+{
+    public Dictionary<uint, uint> CrcTable = new();
+    public Dictionary<string, uint> NameTable = new();
+
+    public static RESTBL Parse(byte[] data)
+    {
+        using BinaryReader br = new(new MemoryStream(data));
+        RESTBL restbl = new();
+
+        byte[] magic = br.ReadBytes(6);
+
+        uint unknown1 = br.ReadUInt32();
+        uint unknown2 = br.ReadUInt32();
+
+        uint crcCount = br.ReadUInt32();
+        uint nameCount = br.ReadUInt32();
+
+        for (int i = 0; i < crcCount; i++) {
+            restbl.CrcTable.Add(br.ReadUInt32(), br.ReadUInt32());
+        }
+
+        for (int i = 0; i < nameCount; i++) {
+            byte[] name = br.ReadBytes(160);
+            restbl.NameTable.Add(Encoding.UTF8.GetString(name), br.ReadUInt32());
+        }
+
+        return restbl;
+    }
+}
--- Totk.Analyze/Program.cs
+++ Totk.Analyze/Program.cs
@@ -1,3 +1,13 @@
-﻿using System.Text.Json;
-using Totk.Analyze;
-using ZstdSharp;
+﻿using Totk.Analyze;
+using Totk.Analyze.Formats;
+
+const string path = "D:\\Bin\\totk\\ResourceSizeTable.Product.100.rsizetable";
+RESTBL restbl = RESTBL.Parse(File.ReadAllBytes(path));
+
+// foreach (var item in restbl.CrcTable) {
+//     Console.WriteLine(item.Key);
+// }
+
+foreach (var item in restbl.NameTable) {
+    Console.WriteLine(item.Key);
+}

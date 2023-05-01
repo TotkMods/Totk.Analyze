c202fb1a4784ecc0c3d4b324c2894933f7da08b6
ArchLeaders <archparliament@gmail.com>
2023-05-01 15:40:09 -0700

    Create ZStd extension methods

---
+++ Totk.Analyze/ZStd.cs
@@ -0,0 +1,17 @@
+﻿using ZstdSharp;
+
+namespace Totk.Analyze;
+
+public class ZStd
+{
+    public static void Decompress(string file)
+    {
+        var src = File.ReadAllBytes(file);
+        using Decompressor decompressor = new();
+        var decompressed = decompressor.Unwrap(src);
+
+        string outputFile = file.Remove(file.Length - 4, 3);
+        FileStream fs = File.Create(outputFile);
+        fs.Write(decompressed);
+    }
+}

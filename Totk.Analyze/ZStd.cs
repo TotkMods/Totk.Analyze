a4a5d90ecf24a87518afe2ed0a624934af976ddd
ArchLeaders <archparliament@gmail.com>
2023-05-01 21:22:17 -0700

    Impl integrated zstd decompression

--- Totk.Analyze/ZStd.cs
+++ Totk.Analyze/ZStd.cs
@@ -1,17 +1,47 @@
-﻿using ZstdSharp;
+﻿using SarcLibrary;
+using ZstdSharp;
 
 namespace Totk.Analyze;
 
 public class ZStd
 {
+    private enum DecompressorMode
+    {
+        Common,
+        Pack,
+        Map
+    }
+
+    private static readonly string _zsDicPath = Path.Combine(Global.Config.GamePath, "Pack", "ZsDic.pack.zs");
+    private static readonly Decompressor _commonDecompressor = LoadDicts(DecompressorMode.Common);
+    private static readonly Decompressor _mapDecompressor = LoadDicts(DecompressorMode.Map);
+    private static readonly Decompressor _packDecompressor = LoadDicts(DecompressorMode.Pack);
+
     public static void Decompress(string file)
     {
-        var src = File.ReadAllBytes(file);
-        using Decompressor decompressor = new();
-        var decompressed = decompressor.Unwrap(src);
+        Span<byte> src = File.ReadAllBytes(file);
+        Span<byte> data =
+            file.EndsWith(".bcett.byml.zs") ? _mapDecompressor.Unwrap(src) :
+            file.EndsWith(".pack.zs") ? _packDecompressor.Unwrap(src) :
+            _commonDecompressor.Unwrap(src);
 
-        string outputFile = file.Remove(file.Length - 4, 3);
-        FileStream fs = File.Create(outputFile);
-        fs.Write(decompressed);
+        FileStream fs = File.Create(file.Remove(file.Length - 3, 3));
+        fs.Write(data);
+    }
+
+    private static Decompressor LoadDicts(DecompressorMode mode)
+    {
+        Decompressor decompressor = new();
+
+        Span<byte> data = decompressor.Unwrap(File.ReadAllBytes(_zsDicPath));
+        SarcFile sarc = SarcFile.FromBinary(data.ToArray()); // use cead to avoid the copy
+
+        decompressor.LoadDictionary(sarc[mode switch {
+            DecompressorMode.Map => "bcett.byml.zsdic",
+            DecompressorMode.Pack => "pack.zsdic",
+            _ => "zs.zsdic"
+        }]);
+
+        return decompressor;
     }
 }

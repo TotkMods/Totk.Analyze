3c66edf3cda022dec6b28c784cf945a4a0dc6bba
ArchLeaders <archparliament@gmail.com>
2023-05-05 11:46:28 -0700

    Create FileProcessor and TotkFileInfo class

---
+++ src/Components/FileProcessor.cs
@@ -0,0 +1,98 @@
+﻿using Cead;
+using Cead.Handles;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Totk.Analyze.Models;
+
+namespace Totk.Analyze.Components;
+
+public unsafe class FileProcessor
+{
+    public Dictionary<string, Dictionary<string, TotkFileInfo>> FileInfo { get; } = new();
+    public List<string> AllExtensions { get; } = new();
+
+    public void Process(string path, nint? src = null, int? len = null)
+    {
+        string relPath = Path.GetRelativePath(TotkConfig.Shared.GamePath, path);
+        Console.WriteLine($"Processing '{relPath}'");
+
+        (string newExtPath, string ext) = GetExt(relPath);
+        TotkFileInfo info = new(path, ext, src, len);
+        Add(ext, relPath, info);
+
+        string outputPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Extensions", ext, newExtPath);
+        Directory.CreateDirectory(Path.GetDirectoryName(outputPath)!);
+
+        try {
+            RunChecks(path, info, outputPath);
+        }
+        catch (Exception ex) {
+            info.TestResult = ex.Message;
+        }
+
+        if (info.TestResult != null) {
+            Console.WriteLine($"  {info.TestResult}");
+        }
+
+        using FileStream fs = File.Create(outputPath + ".json");
+        JsonSerializer.Serialize(fs, info, new JsonSerializerOptions {
+            WriteIndented = true,
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+        });
+    }
+
+    public void RunChecks(string path, TotkFileInfo info, string outputPath)
+    {
+        if (info.Magic == "SARC") {
+            using Sarc sarc = Sarc.FromBinary(info.GetData());
+            foreach ((var name, var file) in sarc) {
+                fixed (byte* ptr = file.AsSpan()) {
+                    Process(Path.Combine(path, name), (nint)ptr, file.AsSpan().Length);
+                }
+            }
+
+            using DataHandle handle = sarc.ToBinary();
+            if (info.GetData().SequenceEqual(handle)) {
+                info.TestResult = "SARC was byte perfect";
+            }
+            else {
+                info.TestResult = $"SARC size diff from original: {info.GetData().Length - handle.AsSpan().Length}";
+
+                // Append a random sarc extension
+                // to fix conflicts with the folder
+                using FileStream fs = File.Create(outputPath + ".bactorpack");
+                fs.Write(handle);
+            }
+        }
+        else if (info.Magic == "YB" || info.Magic == "BY" && !path.EndsWith(".esetb.byml.zs")) {
+            using Byml byml = Byml.FromBinary(info.GetData());
+            using DataHandle handle = byml.ToBinary(false, 7);
+            if (info.GetData().SequenceEqual(handle)) {
+                info.TestResult = "BYML read/write was byte perfect";
+            }
+            else {
+                info.TestResult = $"BYML size diff from original: {info.GetData().Length - handle.AsSpan().Length}";
+                using FileStream fs = File.Create(outputPath);
+                fs.Write(handle);
+            }
+        }
+    }
+
+    private void Add(string ext, string path, TotkFileInfo info)
+    {
+        if (!FileInfo.TryGetValue(ext, out Dictionary<string, TotkFileInfo>? files)) {
+            FileInfo[ext] = files = new();
+        }
+
+        files.Add(path, info);
+    }
+
+    private static (string, string) GetExt(string path)
+    {
+        if (path.EndsWith(".zs")) {
+            path = path[..^3];
+        }
+
+        return (path, Path.GetExtension(path));
+    }
+}
---
+++ src/Models/TotkFileInfo.cs
@@ -0,0 +1,75 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Totk.Analyze.Models;
+
+public unsafe class TotkFileInfo
+{
+    public required string FilePath { get; set; }
+    public required string Extension { get; set; }
+
+    public required bool IsCompressed { get; set; }
+    public required int? CompressedFileSize { get; set; }
+    public required int FileSize { get; set; } = -1;
+    public required string? FourByteBinaryMagicHex { get; set; }
+    public required string? EightByteBinaryMagicHex { get; set; }
+    public required string? Magic { get; set; }
+    public string? TestResult { get; set; }
+
+    private readonly byte* data;
+
+    [SetsRequiredMembers]
+    public TotkFileInfo(string path, string ext, nint? src = null, int? len = null)
+    {
+        FilePath = path;
+        Extension = ext;
+        IsCompressed = path.EndsWith(".zs");
+
+        Span<byte> buffer;
+        if (src is nint _src && len is int _len) {
+            buffer = new((byte*)_src, _len);
+        }
+        else {
+            using FileStream fs = File.OpenRead(path);
+            fs.Read(buffer = new byte[fs.Length]);
+        }
+
+        if (buffer.Length == 0) {
+            TestResult = "Zero-byte file!";
+            return;
+        }
+
+        CompressedFileSize = IsCompressed ? buffer.Length : null;
+
+        Span<byte> raw = IsCompressed ? TotkZstd.Decompress(path, buffer) : buffer;
+        FileSize = raw.Length;
+
+        StringBuilder magic = new();
+        int i = -1; char x;
+        while (char.IsAsciiLetter(x = (char)raw[++i])) {
+            magic.Append(x);
+        }
+
+        if (magic.Length > 0) {
+            Magic = magic.ToString();
+        }
+        else {
+            if (raw.Length >= 4) {
+                FourByteBinaryMagicHex = Convert.ToHexString(raw[..4]);
+            }
+
+            if (raw.Length >= 8) {
+                EightByteBinaryMagicHex = Convert.ToHexString(raw[..8]);
+            }
+        }
+
+        fixed (byte* ptr = raw) {
+            data = ptr;
+        }
+    }
+
+    public Span<byte> GetData()
+    {
+        return new(data, FileSize);
+    }
+}

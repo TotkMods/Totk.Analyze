2468eeece9d94239421cc6530334c3c38078427b
ArchLeaders <archparliament@gmail.com>
2023-05-12 13:28:39 -0700

    Update main()

--- src/Program.cs
+++ src/Program.cs
@@ -1,62 +1,19 @@
 ﻿using Cead.Interop;
-using System.Diagnostics;
-using System.Text;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using Totk.Analyze;
-using Totk.Analyze.Components;
+using CsRestbl;
+using Native.IO.Services;
 using Totk.Analyze.Extensions;
-using Totk.Analyze.Models;
+using Totk.Analyze.Scripts;
 
-const string path = "D:\\Bin\\Totk\\Json\\Totk.Analyze.Info.json";
-var info = JsonSerializer.Deserialize<Dictionary<string, Dictionary<string, TotkFileInfo>>>(File.ReadAllBytes(path))!;
+// This should always be called
+// when the application starts.
+DllManager.LoadCead();
+NativeLibraryManager.RegisterPath("D:\\Bin\\native", out bool isCommonLoaded)
+    .Register(RestblLibrary.Shared, out bool isRestblLoaded);
+JsonExtension.RegisterLocation("D:\\Bin\\Totk");
 
-Dictionary<string, string> extMagic = new();
+Console.WriteLine($"""
+    Common Loaded: {isCommonLoaded}
+    Restbl Loaded: {isRestblLoaded}
+    """);
 
-foreach ((var ext, var entries) in info) {
-    List<string> found = new();
-    foreach ((var _, var tInfo) in entries) {
-        if (tInfo.Magic != null) {
-            found.Add(tInfo.Magic);
-        }
-    }
-
-    if (found.Count > 0) {
-        extMagic.Add(ext, string.Join(',', found.Distinct().Order()));
-    }
-}
-
-using FileStream fs = File.Create("D:\\Bin\\Totk\\Json\\Extensions-Magic.json");
-JsonSerializer.Serialize(fs, extMagic, new JsonSerializerOptions {
-    WriteIndented = true
-});
-
-//DllManager.LoadCead();
-
-//Stopwatch watch = Stopwatch.StartNew();
-//FileProcessor processor = new();
-
-//try {
-//#if DEBUG
-//    new RecursionExtension(processor.Process).IterateFolder(TotkConfig.Shared.GamePath);
-//#else
-//    await new RecursionExtension(processor.Process, parallel: false).IterateFolderAsync(TotkConfig.Shared.GamePath);
-//#endif
-//}
-//catch (Exception ex) {
-//    await Console.Out.WriteLineAsync(ex.ToString());
-//}
-//finally {
-//    using FileStream fsExtensions = File.Create(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Totk.Analyze.Extensions.json"));
-//    JsonSerializer.Serialize(fsExtensions, processor.FileInfo.Keys.Order(), new JsonSerializerOptions() {
-//        WriteIndented = true,
-//        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
-//    });
-//}
-
-//watch.Stop();
-//await Console.Out.WriteLineAsync($"""
-//    Processed in {watch.ElapsedMilliseconds / 1000 / 60} minutes
-//    - Elapsed Milliseconds: {watch.ElapsedMilliseconds}
-//    - Elapsed Ticks: {watch.ElapsedMilliseconds}
-//    """);
+GenHashTable.Execute();

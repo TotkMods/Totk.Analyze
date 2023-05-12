2ee5c347ae6f4b882091ce4ec57d6ee3af1c2ff2
ArchLeaders <archparliament@gmail.com>
2023-05-12 12:54:34 -0700

    Impl json logging extension

---
+++ src/Extensions/JsonExtension.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+
+namespace Totk.Analyze.Extensions;
+
+public static class JsonExtension
+{
+    private static string _path = Path.Combine(AppContext.BaseDirectory, "Stash", "Json");
+
+    public static void StashJson<T>(this T value, string path, bool format = true)
+    {
+        string file = Path.Combine(_path, path);
+        Directory.CreateDirectory(Path.GetDirectoryName(file)!);
+
+        using FileStream fs = File.Create(file);
+        JsonSerializer.Serialize(fs, value, new JsonSerializerOptions {
+            WriteIndented = true
+        });
+    }
+
+    public static T LoadJson<T>(this string path)
+    {
+        using FileStream fs = File.OpenRead(Path.Combine(_path, path));
+        return JsonSerializer.Deserialize<T>(fs)!;
+    }
+
+    public static void RegisterLocation(string path)
+    {
+        _path = path;
+    }
+}

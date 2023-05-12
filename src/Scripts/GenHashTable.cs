3639170a708cce121bc85d7e3bd31dde57e62ed4
ArchLeaders <archparliament@gmail.com>
2023-05-12 13:06:16 -0700

    Create GenHashTable script

---
+++ src/Scripts/GenHashTable.cs
@@ -0,0 +1,43 @@
+﻿using CsRestbl;
+using Totk.Analyze.Extensions;
+
+namespace Totk.Analyze.Scripts;
+
+public static class GenHashTable
+{
+    public static Dictionary<uint, string> HashTable { get; } = new();
+
+    public static void Execute()
+    {
+        using Restbl restbl = Restbl.FromBinary(
+            TotkConfig.GetFileBytes(Path.Combine("System", "Resource", "ResourceSizeTable.Product.110.rsizetable.zs"), decompress: true));
+
+        var fileCache = JsonExtension.LoadJson<Dictionary<string, string?>>("FileCache.json");
+        foreach ((var file, var rel) in fileCache) {
+            string path = (rel != null ? Path.GetRelativePath(rel, file) : file).Canonical();
+            string ext = path.GetExt();
+            string entryName = (ext is ".ta" ? file : path).Replace(Path.DirectorySeparatorChar, '/');
+            uint hash = Crc32.Compute(entryName);
+
+            if (restbl.NameTable.Contains(entryName)) {
+                restbl.NameTable.Remove(entryName);
+            }
+            else if (restbl.CrcTable.Contains(hash)) {
+                restbl.CrcTable.Remove(hash);
+                HashTable.Add(hash, entryName);
+            }
+        }
+
+        HashTable.OrderBy(x => x.Key)
+            .ToDictionary(x => x.Key, x => x.Value)
+            .StashJson(Path.Combine("Hashes", "HashTable.json"));
+
+        using FileStream fs = File.Create("D:\\Bin\\Totk\\Hashes\\Unmatched.rsizetable");
+        fs.Write(restbl.ToBinary());
+    }
+
+    public static string GetExt(this string path)
+    {
+        return Path.GetExtension(path);
+    }
+}

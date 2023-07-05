fed3488f95cdb3504d40ac224fd1e29ad510a855
ArchLeaders <archparliament@gmail.com>
2023-07-05 11:49:24 -0700

    Export strings-totk

--- src/Scripts/GenHashTable.cs
+++ src/Scripts/GenHashTable.cs
@@ -32,6 +32,10 @@ public static class GenHashTable
             .ToDictionary(x => x.Key, x => x.Value)
             .StashJson(Path.Combine("Hashes", "HashTable.json"));
 
+        File.WriteAllLines(Path.Combine("D:", "Bin", "Totk", "Hashes", "TotK-Strings.txt"), HashTable
+            .OrderBy(x => x.Value)
+            .Select(x => x.Value));
+
         using FileStream fs = File.Create("D:\\Bin\\Totk\\Hashes\\Unmatched.rsizetable");
         fs.Write(restbl.ToBinary());
     }
